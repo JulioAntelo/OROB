d79a539f6b16accb791314028798a5f5d2b12066
JulioAntelo <jantram710@g.educaand.es>
2023-01-26 20:16:58 +0100

    subir

--- orientado a objetos/src/main/kotlin/Main.kt
+++ orientado a objetos/src/main/kotlin/Main.kt
@@ -35,28 +35,3 @@ fun main(args: Array<String>) {
 
 
 
-class coche() {
-    var marca:String = ""
-    var color:String =""
-    var modelo:String = ""
-    var caballos:Int=0
-    var num_puertas:Int =0
-    var matricula:String=""
-    fun imprimir(){
-     println("marca: $marca")
-
-     println("color: $color")
-     println("modelo: $modelo")
-     println("caballos: $caballos")
-     println("numero de puertas: $num_puertas")
-     println("matricula:  $matricula")
- }
-
-
-
-}
-
-
-
-
-
---
+++ orientado a objetos/src/main/kotlin/coche.kt
@@ -0,0 +1,20 @@
+class coche() {
+    var marca:String = ""
+    var color:String =""
+    var modelo:String = ""
+    var caballos:Int=0
+    var num_puertas:Int =0
+    var matricula:String=""
+    fun imprimir(){
+        println("marca: $marca")
+
+        println("color: $color")
+        println("modelo: $modelo")
+        println("caballos: $caballos")
+        println("numero de puertas: $num_puertas")
+        println("matricula:  $matricula")
+    }
+
+
+
+}
