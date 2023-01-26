7931918c542ca2837567dc9f207f514ec3728adc
JulioAntelo <jantram710@g.educaand.es>
2023-01-26 19:02:30 +0100

    subir

---
+++ orientado a objetos/src/main/kotlin/libro.kt
@@ -0,0 +1,57 @@
+class libro {
+    var autor:String =""
+        get(){
+            return field
+        }
+        set(value) {
+            field = value
+        }
+    var titulo:String =""
+        get(){
+            return field
+        }
+        set(value) {
+            field = value
+        }
+    var puntuacion:Int = 5
+        get() {
+            if (field<5){
+                println("no ta muy weno la verdad")
+            }
+            return field
+        }
+        set(value) {
+            field = when{
+                value > 10 -> 10
+                value < 0 -> 0
+                else -> value
+            }
+        }
+    var num_paginas:Int = 0
+        get() {
+            return field
+        }
+        set(value) {
+            field = when{
+                value > 3031 -> 3031
+                value < 50 -> 50
+                else -> value
+            }
+        }
+
+    fun imprimir_info(){
+        println(" autor: $autor\n título: $titulo\n puntuacion: $puntuacion\n numero de paginas: $num_paginas ")
+    }
+
+    fun modificar_info(){
+        println("que quieres modificar? \n 1.- autor \n 2.- titulo \n 3.- puntuacion \n 4.- numero de paginas")
+        var seleccion_a_modificar = readLine()!!.toInt()
+        when (seleccion_a_modificar){
+            1 -> autor =  readLine()!!.toString()
+            2 -> titulo =  readLine()!!.toString()
+            3 -> puntuacion =  readLine()!!.toInt()
+            4 -> num_paginas =  readLine()!!.toInt()
+        }
+    }
+
+}
