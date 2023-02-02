83cf74d3e0e7a35868b563d2ad6ee9c5a44ec5eb
JulioAntelo <jantram710@g.educaand.es>
2023-02-02 21:17:46 +0100

    subir

--- orientado a objetos/src/main/kotlin/Tiempo/Día.kt
+++ orientado a objetos/src/main/kotlin/Tiempo/Día.kt
@@ -4,7 +4,7 @@ class Día (Temp_Max_Día: Int, Temp_Min_Día:Int, Estado_Día:String){
     var Temperatura_Mínima = Temp_Min_Día
     var Estado = Estado_Día
         set(value) {
-            val lista = listOf<Char>('S', 'N', 'L')
+            val lista = listOf<String>("S", "N", "L")
             if (value !in lista) {
                 println("Estado del día no válido.")
             } else
