808e79d7e493d527c64982fd7cba67eac91b7cc8
JulioAntelo <jantram710@g.educaand.es>
2023-02-10 20:09:25 +0100

    subir

--- orientado a objetos/src/main/kotlin/Restaurante/Menú.kt
+++ orientado a objetos/src/main/kotlin/Restaurante/Menú.kt
@@ -6,32 +6,28 @@ class Menú(IVA:Double =0.21, Descuento: Double, Principal:String, Bebidas:Strin
     var principal = Principal
     var bebidas = Bebidas
     var postre = Postre
-    var DiccionarioComidas= mutableMapOf("Entrecot" to 15,
-                                "Pez Espada" to 12,
-                                "Carrillada" to 10,
-                                "Dorada" to 20,
-                                "Otros" to 13)
+    var DiccionarioComidas= mutableMapOf("Entrecot" to 15.0,
+                                "Pez Espada" to 12.0,
+                                "Carrillada" to 10.0,
+                                "Dorada" to 20.0,
+                                "Otros" to 13.0)
 
-    var DiccionarioBebidas = mutableMapOf("Refresco" to 1,
+    var DiccionarioBebidas = mutableMapOf("Refresco" to 1.0,
                                             "Vino" to 1.5,
-                                            "Otros" to 2)
+                                            "Otros" to 2.0)
 
-    var DiccionarioPostres = mutableMapOf("Fruta" to 2,
-                                            "Helado" to 3,
-                                            "Otros" to 4)
+    var DiccionarioPostres = mutableMapOf("Fruta" to 2.0,
+                                            "Helado" to 3.0,
+                                            "Otros" to 4.0)
 
     fun importe(): Double {
         var importe = 0
         println("que desea pedir para principal?")
-        importe+=  DiccionarioComidas.getOrDefault(principal,13)
-
-        println("que desea pedir para beber?")
-        importe+=  DiccionarioComidas.getOrDefault(bebidas,2)
-
-        println("que desea pedir de postre?")
-        importe+=  DiccionarioComidas.getOrDefault(postre,4)
-
+        importe =  (DiccionarioComidas.getOrDefault(principal,13) +
+                   DiccionarioBebidas.getOrDefault(bebidas,2) +
+                   DiccionarioPostres.getOrDefault(postre,4))
         var importeConIva = importe + (importe*Iva)
-        return importeConIva
+        return (importeConIva -(importeConIva*descuento))
     }
+
 }
