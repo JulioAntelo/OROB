15ccf72b1601aefd3865ad717311cb6c8ede9405
JulioAntelo <jantram710@g.educaand.es>
2023-02-16 20:44:50 +0100

    subir

--- orientado a objetos/src/main/kotlin/Empresa_Alquiler_Vehiculos/VehiculoCarga.kt
+++ orientado a objetos/src/main/kotlin/Empresa_Alquiler_Vehiculos/VehiculoCarga.kt
@@ -1,4 +1,22 @@
 package Empresa_Alquiler_Vehiculos
 
 class VehiculoCarga {
+    var tara:Int = 0
+        get():Int {
+            return field
+        }
+        set(value) {
+            if (value >1000 || value<1){
+                println("la duración no es correcta")
+            }
+            else{field =value}
+        }
+
+    fun alquiler() {
+
+    }
+
+    fun recibo(){
+
+    }
 }
