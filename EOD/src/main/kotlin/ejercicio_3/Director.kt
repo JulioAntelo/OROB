dcc197f0614b5c98afb764dc09786f90872fc0b2
JulioAntelo <jantram710@g.educaand.es>
2023-02-22 19:01:06 +0100

    subir

--- EOD/src/main/kotlin/ejercicio_3/Director.kt
+++ EOD/src/main/kotlin/ejercicio_3/Director.kt
@@ -1,7 +1,7 @@
 package ejercicio_3
 
 class Director(nombre: String, nomina: Float) :Trabajador(nombre, nomina) {
-     fun costoDelPersonal(): Float {
+     override fun costoDelPersonal(): Float {
         var costoFinal = 0f
          costoFinal = nomina
          return costoFinal
