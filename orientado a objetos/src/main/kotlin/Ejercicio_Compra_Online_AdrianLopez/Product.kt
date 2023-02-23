3a8b0d265cb3b9bdb9a70213674ea04c26d356b9
JulioAntelo <jantram710@g.educaand.es>
2023-02-23 19:50:24 +0100

    subir

--- orientado a objetos/src/main/kotlin/Ejercicio_Compra_Online_AdrianLopez/Product.kt
+++ orientado a objetos/src/main/kotlin/Ejercicio_Compra_Online_AdrianLopez/Product.kt
@@ -16,4 +16,22 @@ abstract class Product(RegInitialPrice:Double) {
 
     abstract fun computeespecialcostumerPrice():Double
 
+    companion object Iva{
+        val iva = intArrayOf(0,4,10,21)
+        init {
+            println("Inicializando el companion")
+        }
+        fun IvaMenor():Int {
+            return iva[0]
+        }
+        fun IvaMeioMenor():Int {
+            return iva[1]
+        }
+        fun IvaMedioMayor():Int {
+            return iva[2]
+        }
+        fun IvaMayor():Int {
+            return iva[3]
+        }
+    }
 }
