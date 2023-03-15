ebf7454839b490901df3ee6f946c5d44f1a51553
JulioAntelo <jantram710@g.educaand.es>
2023-03-15 19:29:46 +0100

    subir

--- EOD/src/main/kotlin/examen_refactorizar/main.kt
+++ EOD/src/main/kotlin/examen_refactorizar/main.kt
@@ -1,6 +1,15 @@
 package examen_refactorizar
 
 fun main() {
+    /**
+     * He quitado la variable de tipo ya que por cada tipo de embarcación
+     * puede ser un objeto con diferentes cualidades, haciendo que alguna
+     * de estas sean iguales y otras no, por lo tanto todas heredan de la
+     * clase embarcación en la cual he creado el método CalcularCosteTotal
+     * para ver el coste de la embarcación en funcion de las horas seleccionadas
+     * para concluir he añadido un toString a cada uno para ver sus datos ya que
+     * la funcion anterior presentaba problemas de codigo duplicado principalmente
+     */
     var kayak1=kayak(5)
     kayak1.toString()
     kayak1.CalcularCosteTotal(7)
