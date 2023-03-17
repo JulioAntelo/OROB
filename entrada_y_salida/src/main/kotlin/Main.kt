d8e990897a5373b529d7cfd0a5cce76c312ef2d4
JulioAntelo <jantram710@g.educaand.es>
2023-03-17 20:02:08 +0100

    a

--- entrada_y_salida/src/main/kotlin/Main.kt
+++ entrada_y_salida/src/main/kotlin/Main.kt
@@ -1,7 +1,89 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import java.io.File
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+
+
+
+fun main(args: Array<String>) {
+
+    fun parte1(){
+        val salario: Double?
+        print("Introduce un salario: ")
+        salario = readLine()?.toDoubleOrNull()
+
+        if (salario == null) {
+            println("ERROR")
+        }
+    }
+    fun parte2(){
+        /*segundo si el salario no es numérico o es nulo muestra un mensaje*/
+        val salario: Double?
+        print("Introduce un salario: ")
+        salario = readLine()?.toDoubleOrNull()
+
+        if (salario != null) {
+            println("El salario es: $salario")
+
+        } else {
+            println("¡Eso no es un salario!")
+        }
+    }
+
+    fun parte3(){
+        var salario: Double?
+        print("Introduce un salario: ")
+        salario = readLine()?.toDoubleOrNull()
+
+        if (salario != null) {
+            println("El salario es: $salario")
+        } else {
+            println("¡Eso no es un salario!")
+            print("Introduce un salario: ")
+            salario = readLine()?.toDoubleOrNull()
+        }
+        if (salario != null) {
+            println("El salario es: $salario")
+
+        } else {
+            println("¡Eso no es un salario!")
+        }
+    }
+
+    fun parte4(){
+        var salario:Double?
+        print("Introduce un salario: ")
+        salario = readLine()?.toDoubleOrNull()
+        while(salario==null){
+            println("¡Eso no es un salario!")
+            print("Introduce un salario: ")
+            salario = readLine()?.toDoubleOrNull()
+        }
+        println("El salario es: $salario")
+    }
+
+    fun parte5(){
+        var salario:Double?
+        print("Introduce un salario: ")
+        salario = readLine()?.toDoubleOrNull()
+        if (salario!=null){
+            println("error")
+        }
+    }
+    val str1 = String.format("%d", 404) // Integer value
+    val str2 = String.format("%s", "Mehmet") // String value
+    val str3 = String.format("%f", 404.00) // Float value
+    val str4 = String.format("%x", 404) // Hexadecimal value
+    val str5 = String.format("%c", 'c') // Char value
+    println(str1)
+    println(str2)
+    println(str3)
+    println(str4)
+    println(str5)
+
+
+
+
+
+
+
 }
+
