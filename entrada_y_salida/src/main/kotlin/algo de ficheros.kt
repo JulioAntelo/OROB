fd2767f4350e6b28d8688659778e11b9f31ab8bd
JulioAntelo <jantram710@g.educaand.es>
2023-04-13 20:06:22 +0200

    a

--- entrada_y_salida/src/main/kotlin/algo de ficheros.kt
+++ entrada_y_salida/src/main/kotlin/algo de ficheros.kt
@@ -4,7 +4,7 @@ import java.io.File
 fun main(args: Array<String>) {
 
     fun main(args: Array<String>) {
-        val file = File("C:\\Users\\usuario tarde\\Desktop\\cosas con el itellij\\entrada_y_salida" + File.separator + "patatas.txt")
+        val file = File("patatas.txt")
         val bufferedReader = file.bufferedReader()
         val text: List<String> = bufferedReader.readLines()
         for (line in text) {
